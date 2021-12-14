a0e23d48575a1f9dcf2ebdbd1a63a9db915b6afa
Cupofspit <adonaghy@oakland.edu>
2021-12-13 18:58:59 -0800

    fifth commit

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
-// //npm install express
-// const express = require('express');
-// //initialize express framework
-// const app = express();
-// //create “ENDPOINTS” aka route handlers
-// app.get('/', function (req,res) {
-// 	res.send('Hello world from express.js');
-// });
-// const PORT = process.env.PORT || 3000;
-// //start server on a PORT
-// app.listen(3000, ()=> console.log(`Server running on 3000`));
+//npm install express
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const {request} = require('express');
 
 
-//imports
-const express = require('express')
-const app = express()
-const port = 3000
 
-//Static files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/fonts', express.static(__dirname + 'public/fonts'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/scss', express.static(__dirname + 'public/scss'))
-
-//Set views
-app.set('views', './views')
-app.set('views engine', 'ejs')
+//initialize express framework
+const app = express();
 
 
-app.get('', (req,res) => {
-	res.render('index', { text: 'This is EJS'})
-})
+//initialize body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/gallery', (req,res) => {
-	res.render('gallery', { text: 'Gallery page'})
-})
+//initialize ejs middleware
+app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
+
+
+//create “ENDPOINTS” aka route handlers
+app.get('/', (req,res) => {
+	res.render('index');
+});
+
+//nav routes
+app.get('/views/index.ejs', (req,res) => {
+	res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+//start server on a PORT
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+
+// //imports
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// //Static files
+// app.use(express.static('public'))
+// app.use('/css', express.static(__dirname + 'public/css'))
+// app.use('/js', express.static(__dirname + 'public/js'))
+// app.use('/fonts', express.static(__dirname + 'public/fonts'))
+// app.use('/img', express.static(__dirname + 'public/img'))
+// app.use('/scss', express.static(__dirname + 'public/scss'))
+
+// //Set views
+// app.set('views', './views')
+// app.set('views engine', 'ejs')
+
+
+// app.get('', (req,res) => {
+// 	res.render('index', { text: 'This is EJS'})
+// })
+
+// app.get('/gallery', (req,res) => {
+// 	res.render('gallery', { text: 'Gallery page'})
+// })
+
