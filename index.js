a6c1ce5394c32ce8e09ec765de3ff2067ff813c4
Cupofspit <adonaghy@oakland.edu>
2021-12-13 21:15:58 -0800

    10th commit

--- index.js
+++ index.js
@@ -48,14 +48,6 @@ app.get('/views/404.ejs', (req,res) => {
 	res.render('/404.ejs');
 });
 
-if (process.env.NODE_ENV === "production"{
-	app.use(express.static("build"));
-	app.get("*", (req, res) => {
-	  res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-	});
-  }
-
-
 const PORT = process.env.PORT || 3000;
 //start server on a PORT
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
