8ccfe9f076556e26a528a9b423c3ec9ae891f8f5
Cupofspit <adonaghy@oakland.edu>
2021-12-13 23:34:37 -0800

    12.0 commit (fixed pathing)

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use(express.urlencoded({extended: true}));
 //initialize ejs middleware
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
-// app.use('/css', express.static(__dirname + 'public/css'));
-// app.use('/js', express.static(__dirname + 'public/js'));
-// app.use('/fonts', express.static(__dirname + 'public/fonts'));
-// app.use('/img', express.static(__dirname + 'public/img'));
-// app.use('/scss', express.static(__dirname + 'public/scss'));
-// app.use('/views', express.static(__dirname + '/views'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/fonts', express.static(__dirname + 'public/fonts'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/scss', express.static(__dirname + 'public/scss'));
+app.use('', express.static(__dirname + ''));
 
 //create “ENDPOINTS” aka route handlers
 app.get('/', (req,res) => {
@@ -29,23 +29,23 @@ app.get('/', (req,res) => {
 });
 
 //nav routes
-app.get('/views/index.ejs', (req,res) => {
+app.get('/index', (req,res) => {
 	res.render('index');
 });
-app.get('/views/contact.ejs', (req,res) => {
-	res.render('./contact.ejs');
+app.get('/contact', (req,res) => {
+	res.render('contact');
 });
-app.get('/views/gallery.ejs', (req,res) => {
+app.get('/gallery', (req,res) => {
 	res.render('gallery');
 });
-app.get('/views/blog-single.ejs', (req,res) => {
-	res.render('./blog-single.ejs');
+app.get('/blog-single', (req,res) => {
+	res.render('blog-single');
 });
-app.get('/views/blog-archive.ejs', (req,res) => {
-	res.render('./blog-archive.ejs');
+app.get('/blog-archive', (req,res) => {
+	res.render('blog-archive');
 });
-app.get('/views/404.ejs', (req,res) => {
-	res.render('./404.ejs');
+app.get('/404', (req,res) => {
+	res.render('404');
 });
 
 const PORT = process.env.PORT || 3000;
