ac801fb1e285b72f840f8e8d6d945f9c465a8312
Cupofspit <adonaghy@oakland.edu>
2021-12-13 22:57:22 -0800

    11.2 commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/views/contact.ejs', (req,res) => {
 	res.render('./contact.ejs');
 });
 app.get('/views/gallery.ejs', (req,res) => {
-	res.render('./gallery.ejs');
+	res.render('gallery');
 });
 app.get('/views/blog-single.ejs', (req,res) => {
 	res.render('./blog-single.ejs');
@@ -53,27 +53,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 
-// //imports
-// const express = require('express')
-// const app = express()
-// const port = 3000
 
-// //Static files
-// app.use(express.static('public'))
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/fonts', express.static(__dirname + 'public/fonts'))
-// app.use('/img', express.static(__dirname + 'public/img'))
-// app.use('/scss', express.static(__dirname + 'public/scss'))
-
-// //Set views
-// app.set('views', './views')
-// app.set('views engine', 'ejs')
-
-
-// app.get('', (req,res) => {
-// 	res.render('index', { text: 'This is EJS'})
-// })
 
 // app.get('/gallery', (req,res) => {
 // 	res.render('gallery', { text: 'Gallery page'})
